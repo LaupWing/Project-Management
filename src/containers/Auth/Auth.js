39fac23adec7523b7882e3cf5edeb555265ea1fc
LaupWing <loc_strict@hotmail.com>
2020-01-29 20:19:17 +0100

    base styling

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Login from './Login/Login'
 import Signup from './Signup/Signup'
+import classes from './Auth.module.css'
 
 class Auth extends React.Component{
     render(){
         return(
-            <div>
+            <div className={classes.Auth}>
                 <Login/>
                 <Signup/>
             </div>
