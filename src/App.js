0a20b0f6e97de0a41f21129458f4527e4ced9471
LaupWing <loc_strict@hotmail.com>
2020-01-29 11:56:21 +0100

    added rotues

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom'
+import Auth from './containers/Auth/Auth'
+import Overview from './containers/Overview/Overview'
+
 class App extends Component {
     render(){
+        let routes =(
+            <Switch>
+                <Route path='/auth' component={Auth}/>
+                <Route path='/' component={Overview}/>
+            </Switch>
+        )
         return (
-          <div className="App">
-            
-          </div>
+            <div className="App">
+                {routes}       
+            </div>
         )
     }
 }
