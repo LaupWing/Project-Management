72682d154347072a9eecdaf16d35b7ef1f7d9186
LaupWing <loc_strict@hotmail.com>
2020-01-30 19:14:50 +0100

    Upload

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,7 +1,8 @@
 import React from 'react'
- 
+import classes from './Button.module.css' 
+
 export default (props)=>(
-    <button className={props.classes}>
-        {props.content}
+    <button className={`${classes.Button} ${classes[props.classes]}`}>
+        {props.children}
     </button>
 )
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Login from './Login/Login'
 import Signup from './Signup/Signup'
 import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
 
 class Auth extends React.Component{
     render(){
         return(
             <div className={classes.Auth}>
                 <nav>
+                    <Button classes={'switch-auth'}>
+                        Login
+                    </Button>
                 </nav>
                 <Login/>
                 <Signup/>
