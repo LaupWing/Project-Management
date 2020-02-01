e70639532d1cd4faf4592ac2abcbbc4425f14390
LaupWing <loc_strict@hotmail.com>
2020-02-01 02:01:18 +0100

    added the zero state name screen

--- src/containers/Overview/Projects/Projects.js
+++ src/containers/Overview/Projects/Projects.js
@@ -1,10 +1,53 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import styles from './Projects.module.css'
+import Button from '../../../components/UI/Button/Button'
+import CSSTransition from 'react-transition-group/CSSTransition'
 
 class Projects extends React.Component{
+    state = {
+        name: ''
+    }
+    submitName(e){
+        e.preventDefault()
+        console.log(this.state.name)
+    }
     render(){
         return(
-            <h2 onClick={()=>console.log(this.props.userData)}>Projects</h2>
+            <React.Fragment>
+                {this.props.userData 
+                    ? this.props.userData.type === 'NOT FOUND'
+                            ?   <form onSubmit={this.submitName.bind(this)} className={styles.enterName}>
+                                    <div className={styles.wrapper}>
+                                        <h2>Hello there,</h2>
+                                        <input 
+                                            spellCheck='false' 
+                                            className={styles} 
+                                            type='text' 
+                                            placeholder="Enter your name..."
+                                            value={this.state.name}
+                                            onChange={(e)=>this.setState({name:e.target.value})}
+                                        ></input>
+                                        <CSSTransition
+                                            in={this.state.name.length > 0 }
+                                            timeout={500}
+                                            unmountOnExit
+                                            mountOnEnter
+                                            classNames={{
+                                                enter: '',
+                                                enterActive: styles["button-enter"],
+                                                exitActive: styles["button-exit"],
+                                                exit: '',
+                                            }}
+                                        >
+                                            <Button classes={['enterName']}>Submit</Button>
+                                        </CSSTransition>
+                                    </div>
+                                </form>
+                        : <p>found</p>
+                    : null
+                }
+            </React.Fragment>
         )
     }
 }
