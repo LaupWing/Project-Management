4f29eade30d44f727f4b03d555c594b79bd827cc
LaupWing <loc_strict@hotmail.com>
2020-02-03 00:47:30 +0100

    added color for gradients

--- src/containers/Overview/Projects/ProjectOverview/ProjectOverview.js
+++ src/containers/Overview/Projects/ProjectOverview/ProjectOverview.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import Header from './Header/Header'
 import styles from './ProjectOverview.module.css'
+import Color from 'color'
 
+const color = Color('#08E8DE')
+console.log(color.hsl().hex())
+console.log(color.hsl().rotate(-25).hex())
+console.log(color.hsl().lighten(0.5).hex())
 export default (props)=>{
     return (
         <div className={styles.ProjectOverview}>
