0f73f2d9706ba146f2ff6a016aab6d8a12c079f8
LaupWing <loc_strict@hotmail.com>
2020-02-02 18:53:52 +0100

    added icon and search bar

--- src/containers/Overview/Projects/ProjectOverview/ProjectOverview.js
+++ src/containers/Overview/Projects/ProjectOverview/ProjectOverview.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Header from './Header/Header'
+import styles from './ProjectOverview.module.css'
 
 export default (props)=>{
     return (
-        <div>
+        <div className={styles.ProjectOverview}>
             <Header name={props.userData.displayName}/>
         </div>
     )
