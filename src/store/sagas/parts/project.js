14f06b19a291c1061a2f0288d9b61a1aa46b2edb
LaupWing <loc_strict@hotmail.com>
2020-02-10 19:05:52 +0100

    need seperate error object

--- src/store/sagas/parts/project.js
+++ src/store/sagas/parts/project.js
@@ -1,11 +1,23 @@
 // import firebase from 'firebase'
-// import {put} from 'redux-saga/effects'
-// import * as actions from '../../actions/index'
-// import db from '../../../initFirebase'
+import {put} from 'redux-saga/effects'
+import * as actions from '../../actions/index'
+import db from '../../../initFirebase'
 
 export function* fetchingUserProjects(action){
     const {id} = action
-    yield console.log(id)
+    try{
+        const projects = yield db
+            .collection('projects')
+            .doc(id)
+            .get()
+        if(projects.exists){
+            console.log('exists')
+        }else{
+            yield put(actions.setUserProjects([]))
+        }
+    }catch(e){
+        yield console.log('error')
+    }
 }
 
 // try{
