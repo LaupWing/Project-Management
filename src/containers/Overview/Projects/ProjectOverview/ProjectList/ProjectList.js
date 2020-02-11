d3d9e4991398ef7b8344739311032da8a4eb9f91
LaupWing <loc_strict@hotmail.com>
2020-02-11 21:47:22 +0100

    added add button

--- src/containers/Overview/Projects/ProjectOverview/ProjectList/ProjectList.js
+++ src/containers/Overview/Projects/ProjectOverview/ProjectList/ProjectList.js
@@ -2,11 +2,11 @@ import React from 'react'
 import styles from './ProjectList.module.css'
 import Project from './Project/Project'
 import ShowMore from './Project/ShowMore'
-
+import Button from '../../../../../components/UI/Button/Button'
 export default (props)=>{
     return(
         <section className={styles.ProjectList}>
-            <h2>Projects <span>({props.projects.length})</span></h2>
+            <h2>Projects <span>({props.projects.length})</span> <Button classes={['addProject']}>Button</Button></h2>
 
             <main>
                 {props.projects && props.projects.slice(0,props.limit).map((project,id)=>{
