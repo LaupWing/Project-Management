cbb3f7c589d983d02fd0e1ff0a2c8b3049b8b37c
LaupWing <loc_strict@hotmail.com>
2020-02-19 17:15:12 +0100

    assigned the states to the right inptus

--- src/components/AddProject/AddProject.js
+++ src/components/AddProject/AddProject.js
@@ -4,7 +4,7 @@ import Button from '../UI/Button/Button'
 
 export default props =>{
     const [title, setTitle] = React.useState('')
-    const [description, setdescription] = React.useState('')
+    const [description, setDescription] = React.useState('')
     const [color, setColor] = React.useState('')
 
     const handleSubmit = (e)=>{
@@ -12,9 +12,23 @@ export default props =>{
     }
     return(
         <form onSubmit={handleSubmit} className={styles.AddProject}>
-            <input className={styles.title} type="text" placeholder="Your new Project Title"></input>
-            <textarea placeholder="description"></textarea>
-            <div className={styles.fieldColor}>
+            <input 
+                className={styles.title} 
+                type="text"
+                value={title}
+                onChange={(e)=>setTitle(e.target.value)} 
+                placeholder="Your new Project Title"
+            ></input>
+            <textarea 
+                placeholder="description"
+                value={description}
+                onChange={(e)=>setDescription(e.target.value)}
+            ></textarea>
+            <div 
+                className={styles.fieldColor}
+                value={color}
+                onChange={(e)=>setColor(e.target.value)}
+            >
                 <div className={styles.colorPreview}>
                     <p>Icon Preview</p>
                 </div>
