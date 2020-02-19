428037a96d51601a42b14b55b66f087c0e1a196c
LaupWing <loc_strict@hotmail.com>
2020-02-19 16:49:45 +0100

    added textarea

--- src/components/AddProject/AddProject.js
+++ src/components/AddProject/AddProject.js
@@ -5,6 +5,7 @@ export default props =>{
     return(
         <div className={styles.AddProject}>
             <input className={styles.title} type="text" placeholder="Your new Project Title"></input>
+            <textarea placeholder="description"></textarea>
         </div>
     )
 }
