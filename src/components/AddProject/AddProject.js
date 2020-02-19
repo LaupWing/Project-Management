56153df2cca189a0bff550a0810da51b6f3b1f06
LaupWing <loc_strict@hotmail.com>
2020-02-19 17:12:50 +0100

    added base states

--- src/components/AddProject/AddProject.js
+++ src/components/AddProject/AddProject.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import styles from './AddProject.module.css'
 import Button from '../UI/Button/Button'
+
 export default props =>{
+    const [title, setTitle] = React.useState('')
+    const [description, setdescription] = React.useState('')
+    const [color, setColor] = React.useState('')
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+    }
     return(
-        <form className={styles.AddProject}>
+        <form onSubmit={handleSubmit} className={styles.AddProject}>
             <input className={styles.title} type="text" placeholder="Your new Project Title"></input>
             <textarea placeholder="description"></textarea>
             <div className={styles.fieldColor}>
