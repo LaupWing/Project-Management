94733c06687d9f347433be8a25c5846ad16728b1
LaupWing <loc_strict@hotmail.com>
2020-02-20 14:47:54 +0100

    added more component

---
+++ src/components/UI/More/More.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './More.module.css'
+
+export default props =>(
+    <div className={styles.More}>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+    </div>
+)
