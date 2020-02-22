668de33efe341d5d1d0ea0563152ac803d025f2a
LaupWing <loc_strict@hotmail.com>
2020-02-22 15:05:36 +0100

    succesfully rendering the right component on routes

--- src/containers/Overview/Details/Projects/Project.js
+++ src/containers/Overview/Details/Projects/Project.js
@@ -3,15 +3,20 @@ import styles from './Project.module.css'
 import {connect} from 'react-redux'
 import Detail from './Detail/Detail'
 import Latest from './Latest/Latest'
+import {Switch, Route} from "react-router-dom";
 
 class ProjectDetail extends React.Component{
     render(){
         return(
             <div className={styles.ProjectDetail}>
-                {!this.props.activeProject 
+                <Switch>
+                    <Route exact path='/projects/:id' component={Latest}/>
+                </Switch>
+                <h2>Test</h2>
+                {/* {!this.props.activeProject 
                     ?   <Latest/>
                     :   <Detail project={this.props.activeProject}/>
-                }
+                } */}
             </div>
         )
     }
