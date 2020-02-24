438ce66cb3c41ad21a1e931e5d9b2b060636d602
LaupWing <loc_strict@hotmail.com>
2020-02-24 15:07:20 +0100

    added warning base

--- src/components/UI/Popups/Warning/Warning.js
+++ src/components/UI/Popups/Warning/Warning.js
@@ -4,7 +4,10 @@ import styles from './Warning.module.css'
 export default props =>{
     return (
         <div className={styles.Warning}>
-
+            <p>{props.message}</p>
+            {props.buttons && props.buttons.map(btn=>{
+                return <button className={styles[btn.type]}>{btn.text}</button>
+            })}
         </div>
     )
 }
