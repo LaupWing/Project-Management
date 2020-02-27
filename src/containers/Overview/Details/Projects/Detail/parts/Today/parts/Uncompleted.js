c6793729ed28fde66c357e4aba214dd476f9860b
LaupWing <loc_strict@hotmail.com>
2020-02-27 14:03:30 +0100

    update

--- src/containers/Overview/Details/Projects/Detail/parts/Today/parts/Uncompleted.js
+++ src/containers/Overview/Details/Projects/Detail/parts/Today/parts/Uncompleted.js
@@ -3,6 +3,10 @@ import styles from './Uncompleted.module.css'
 
 export default props =>{
     return (
-        <div className={styles.Uncompleted}>Uncompleted</div>
+        <div className={styles.Uncompleted}>
+            <div className={styles.container}>
+                <h2>Uncompleted Tasks</h2>
+            </div>
+        </div>
     )
 }
