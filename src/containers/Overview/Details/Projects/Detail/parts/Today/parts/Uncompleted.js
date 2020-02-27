a3335e2690d2ad143a1a98107305caec15175e25
LaupWing <loc_strict@hotmail.com>
2020-02-27 14:43:15 +0100

    added the uncompleted

--- src/containers/Overview/Details/Projects/Detail/parts/Today/parts/Uncompleted.js
+++ src/containers/Overview/Details/Projects/Detail/parts/Today/parts/Uncompleted.js
@@ -3,11 +3,17 @@ import styles from './Uncompleted.module.css'
 
 export default props =>{
     console.log(props)
+    const uncompleted = props.tasks && props.tasks.map(task=>
+    <div>{task.task}</div>
+    )
     return (
         <div className={styles.Uncompleted}>
             <div className={styles.container}>
                 <h2>Uncompleted Tasks</h2>
                 <p>You have some uncompleted tasks! What do you want to do with them.</p>
+                <div className={styles.taskWrapper}>
+                    {uncompleted}
+                </div>
             </div>
         </div>
     )
