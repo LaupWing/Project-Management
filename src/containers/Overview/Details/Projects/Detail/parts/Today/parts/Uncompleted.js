71b85f59166b190f4e162560ab4043f6fadbec55
LaupWing <loc_strict@hotmail.com>
2020-02-27 14:06:45 +0100

    added the main styling for the uncompleted

--- src/containers/Overview/Details/Projects/Detail/parts/Today/parts/Uncompleted.js
+++ src/containers/Overview/Details/Projects/Detail/parts/Today/parts/Uncompleted.js
@@ -6,6 +6,7 @@ export default props =>{
         <div className={styles.Uncompleted}>
             <div className={styles.container}>
                 <h2>Uncompleted Tasks</h2>
+                <p>You have some uncompleted tasks! What do you want to do with them.</p>
             </div>
         </div>
     )
