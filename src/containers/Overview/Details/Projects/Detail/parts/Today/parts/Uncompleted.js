464f4d832b12046eed2e74aaa643300e878cb3f7
LaupWing <loc_strict@hotmail.com>
2020-02-27 16:07:08 +0100

    task

--- src/containers/Overview/Details/Projects/Detail/parts/Today/parts/Uncompleted.js
+++ src/containers/Overview/Details/Projects/Detail/parts/Today/parts/Uncompleted.js
@@ -4,13 +4,20 @@ import styles from './Uncompleted.module.css'
 export default props =>{
     console.log(props)
     const uncompleted = props.tasks && props.tasks.map(task=>
-    <div>{task.task}</div>
+        <div className={styles.task}>
+            <p className={styles.name}>{task.task}</p>
+            <div className={styles.options}>
+                <p>Move to:</p>
+                <button>Today</button>
+                <button>Future</button>
+            </div>
+        </div>
     )
     return (
         <div className={styles.Uncompleted}>
             <div className={styles.container}>
                 <h2>Uncompleted Tasks</h2>
-                <p>You have some uncompleted tasks! What do you want to do with them.</p>
+                <p className={styles.description}>You have some uncompleted tasks! What do you want to do with them.</p>
                 <div className={styles.taskWrapper}>
                     {uncompleted}
                 </div>
